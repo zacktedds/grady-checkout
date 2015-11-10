1a5753ea539196f5b0d0b422f2c57df05de2ec78
Zack Tedders <ztedders@uga.edu>
2015-11-10 12:20:00 -0500

    Work completed in class
    Created home login screen, began work on ViewController and data model
    for equipment.

---
+++ GradyCheckout/EquipData.swift
@@ -0,0 +1,15 @@
+//
+//  EquipCategories.swift
+//  GradyCheckout
+//
+//  Created by Zack Tedders on 11/10/15.
+//  Copyright © 2015 Practically Vampires. All rights reserved.
+//
+
+import Foundation
+
+struct EquipData {
+    let equipment = [
+        
+    ]
+}
--- GradyCheckout/ViewController.swift
+++ GradyCheckout/ViewController.swift
@@ -3,23 +3,31 @@
 //  GradyCheckout
 //
 //  Created by Zack Tedders on 11/5/15.
-//  Copyright © 2015 Bridging Technologies. All rights reserved.
+//  Copyright © 2015 Practically Vampires. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func userNameField(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func userIdField(sender: AnyObject) {
+    
+    }
+    @IBAction func beginCheckoutButton() {
+    
+    }
 }
 
